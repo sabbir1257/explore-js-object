d7f834076e5247b8594ecfea8557c0f8bb7c1c27
sabbir1257 <masabbir1257@gmail.com>
2023-02-28 03:30:14 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,32 @@
+//1. create object using object literals
+const player = {};
+player.name = 'Sabbir Sarker';
+player.specialty = 'batsman';
+player.bat = function(){
+    console.log('swing your bat');
+}
+// console.log(player);
+
+// player.bat();
+
+
+const student = {
+    name: 'Sakib Sarker',
+    job: 'web',
+    ball: function(){
+        console.log('throw the ball');
+    },
+    salary: 10000
+}
+
+// console.log(student);
+// student.ball();
+
+
+//2. object constructor 
+const person = new Object();
+console.log(person);
+
+
+// 3. object create method
+ 
